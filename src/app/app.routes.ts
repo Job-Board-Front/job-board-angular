4e448357999fa8e0d833ed705f8347721def5fdd
unknown <ghoumamaram@gmail.com>
2025-12-28 20:53:40 +0100

    job details page static

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Details', component: JobDetailsComponent }
+];
---
+++ src/app/job-details/job-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-details',
+  standalone: true,
+  templateUrl: './job-details.component.html'
+})
+export class JobDetailsComponent {}
