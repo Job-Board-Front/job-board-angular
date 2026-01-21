5f340e8effaffbcfa5a6188c762424f2a0b1729c
maram-br <maram.benrhouma@insat.ucar.tn>
2026-01-21 20:29:52 +0100

    pages folder out of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Jobs } from './pages/jobs/jobs';
-import { authRoutes } from './components/pages/auth/routes/auth.routes';
+import { authRoutes } from './pages/auth/routes/auth.routes';
 import { TestPageComponent } from './components/test-page.component';
 
-import { JobDetailsComponent } from './components/pages/job-details/job-details.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
 
 export const routes: Routes = [
     { path: "", component: Home },
