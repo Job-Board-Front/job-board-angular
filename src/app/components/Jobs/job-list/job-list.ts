195eb65d81fe015b8c17ad0f63c91d6acf78cb96
maram-br <maram.benrhouma@insat.ucar.tn>
2026-01-22 21:22:44 +0100

    update instead of set (signal)

--- src/app/components/Jobs/job-list/job-list.ts
+++ src/app/components/Jobs/job-list/job-list.ts
@@ -53,14 +53,14 @@ export class JobList {
     });
     }
     private loadMoreJobs() {
-      if (!this.lastCursor || this.jobsResource.isLoading()|| !this.hasMoreJobs()) return;
-
-      this.filterSignal.set({
-        ...this.filterSignal(),
+     if (!this.lastCursor || this.jobsResource.isLoading()|| !this.hasMoreJobs()) return;
+     
+      this.filterSignal.update(current => ({
+        ...current,
         cursor: this.lastCursor,
         limit: this.limit,
-      });
-      
+      }));
+
       console.log('Loading more jobs with cursor:', this.lastCursor);
     }
 
