deee9d92e1ad3a18a33a6fd8b17e43cfe6b2c3bc
unknown <ghoumamaram@gmail.com>
2026-01-28 11:59:50 +0100

    job components added

---
+++ src/app/components/Jobs/job-details/job-description/job-description.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-job-description',
+  standalone: true,
+  templateUrl: './job-description.component.html',
+  styleUrl: './job-description.component.css',
+})
+export class JobDescriptionComponent {
+  description = input.required<string>();
+  readMore = signal(false);
+
+  toggleReadMore() {
+    this.readMore.update(v => !v);
+  }
+}
--- src/app/pages/test/test-page/test-page.component.ts
+++ src/app/pages/test/test-page/test-page.component.ts
@@ -73,3 +73,4 @@ export class TestPageComponent {
     console.log('Open', id);
   }
 }
+*/
