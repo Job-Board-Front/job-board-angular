cf08b0fa2376e1368ae48daa474aeb349d9bdd75
loueysioua <sioua.louey@gmail.com>
2026-02-02 21:19:18 +0100

    fix: job decription read more / less

--- src/app/components/Jobs/job-details/job-description/job-description.component.ts
+++ src/app/components/Jobs/job-details/job-description/job-description.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, linkedSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-job-description',
@@ -10,8 +10,16 @@ import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core
 export class JobDescriptionComponent {
   description = input.required<string>();
   readMore = signal(false);
+  descriptionMoreLess = linkedSignal({
+    source: this.description,
+    computation: () => {
+      const words = this.description().trim().split(' ');
+      return `${words.slice(25).join(' ').normalize()}`;
+    },
+  });
 
   toggleReadMore() {
-    this.readMore.update(v => !v);
+    this.readMore.update((v) => !v);
+    this.descriptionMoreLess.update(() => this.description());
   }
 }
--- src/app/components/Jobs/job-details/job-header/job-header.component.ts
+++ src/app/components/Jobs/job-details/job-header/job-header.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Job } from '@/app/interfaces/api/job.models';
 import { FormatDatePipe } from '@/app/pipes/job-details-pipes/format-date.pipe';
 import { GetInitialsPipe } from '@/app/pipes/job-details-pipes/get-initials.pipe';
-import { AbsoluteUrlPipe } from '@/app/pipes/absolute-url.pipe';
 import { LucideAngularModule, MapPin, Briefcase, DollarSign } from 'lucide-angular';
 import { AbsoluteUrlPipe } from '@/app/pipes/absolute-url.pipe';
 
